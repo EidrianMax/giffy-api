fea6065838a8dd190305a8ae206874fc25aaace9
Adrian <eidrian.uct@gmail.com>
2023-08-09 14:19:41 +0200

    change name of the file addFav.js to add-fav.js

--- logic/index.js
+++ logic/index.js
@@ -1,7 +1,7 @@
 import registerUser from './register-user.js'
 import loginUser from './login-user.js'
 import getAllFavs from './getAllFavs.js'
-import addFav from './addFav.js'
+import addFav from './add-fav.js'
 import deleteFav from './deleteFav.js'
 
 export {
