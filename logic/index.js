7e9e75d744d0b6fbcccb9b8ec803ba690878757f
Adrian <eidrian.uct@gmail.com>
2023-08-09 16:51:29 +0200

    change name of the file getAllFavs.js to get-all-favs.js

--- logic/index.js
+++ logic/index.js
@@ -1,6 +1,6 @@
 import registerUser from './register-user.js'
 import loginUser from './login-user.js'
-import getAllFavs from './getAllFavs.js'
+import getAllFavs from './get-all-favs.js'
 import addFav from './add-fav.js'
 import deleteFav from './deleteFav.js'
 
