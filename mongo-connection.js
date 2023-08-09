3f383f2b2c5ff3590f7f3a8b8ec2de2721535912
Adrian <eidrian.uct@gmail.com>
2023-08-09 13:27:46 +0200

    refactor mongo conection for use user collection instead of db

--- mongo-connection.js
+++ mongo-connection.js
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb'
 
 const { NODE_ENV, MONGO_DB_URI } = process.env
 
-export const client = new MongoClient(MONGO_DB_URI)
+const client = new MongoClient(MONGO_DB_URI)
 
 let connection
 
@@ -16,4 +16,10 @@ const db = NODE_ENV === 'test'
   ? connection.db('giffy-test')
   : connection.db('giffy')
 
-export default db
+const usersCollection = db.collection('users')
+
+export {
+  client,
+  db,
+  usersCollection
+}
