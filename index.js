e4c48cd8bb5769c585cb18827597971106f2c23f
Adrian <eidrian.uct@gmail.com>
2023-08-10 16:40:32 +0200

    install cors and accept all origins

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import './load-enviroment.js'
 import router from './route.js'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/', router)
 
